b4c12b9c4b49c0626f35b46fa8a6edab146a1c76
prince214 <princeparaste78@gmail.com>
2023-04-09 19:47:58 +0530

    initial commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AnimatedLogo from "../assets/images/logo-animated.gif";
+
+export default function Loader() {
+  return (
+    <div className="loader">
+      <img className="logo" src={AnimatedLogo} alt="apple loader" />
+    </div>
+  );
+}
